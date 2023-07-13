d71f420aa0599753d5bff3d4f157659ea842b88f
tjjfvi <t6@t6.fyi>
2023-07-13 10:00:29 -0400

    implement rune solution

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import rune from "./solutions/rune/solution.js"
 import rxjsTests from "./solutions/rxjs/solution.js"
 import vanilla from "./solutions/vanilla.js"
 
 await rxjsTests()
 await vanilla()
+await rune()
